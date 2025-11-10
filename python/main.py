39dfc13b65d285c8918df459ead881b7470f5f7f
ahmedwoye@gmail.com Woye1234@@ <ahmedwoye@gmail.com>
2025-11-10 12:46:23 +0000

    adding web and ptyhon artifacts

---
+++ python/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_metthods=["*"],
+    allow_headers=["*"],
+
+)
+
+
+fruits =  [
+
+    {"name" : "Apple", "price": 1.2},
+    {"name" : "Banana", "price": 0.8},
+    {"name" : "Pineapple", "price": 2.2},
+    {"name" : "Cherry", "price": 1.2},
+    {"name" : "Mango", "price": 4.2}
+]
+
+
+
+@app.get("/fruits")
+def get_fruits():
+    return {"fruits": fruits}
